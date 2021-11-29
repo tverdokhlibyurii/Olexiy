71f70da3497b8866398def0956455d3eff39ce33
Yurii <tverdokhlibyurii@gmail.com>
2021-11-29 14:45:14 +0200

    third0 commito

--- angular-petProject-master/src/app/app-routing.module.ts
+++ angular-petProject-master/src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule) },
+  { path: '', redirectTo: '/goods', pathMatch: 'full' },
+  { path: 'goods', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule) },
   { path: 'shopping-list', loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule) },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: 'oplata', loadChildren: () => import('./oplata/oplata.module').then(m => m.OplataModule) }
--- angular-petProject-master/src/app/oplata/oplata.component.ts
+++ angular-petProject-master/src/app/oplata/oplata.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-oplata',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./oplata.component.scss']
 })
 export class OplataComponent implements OnInit {
+  isLoading = false;
+  isLoad = false;
+  timerId: any;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    form.reset();
+  }
+
+  pay(){
+    this.timerId = setTimeout(()=>{
+    this.isLoading = !this.isLoading},100)
+
+    this.timerId = setTimeout(()=>{
+      this.isLoading = !this.isLoading, this.isLoad =!this.isLoad},1000)
+
+    // this.timerId = setTimeout(()=>{
+    //   console.log('dsadsa')},3000)
+
+
+  }
 }
